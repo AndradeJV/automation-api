e4a771bda2277c361c4c584a81a209604d32d4a6
João Vitor Andrade de Araujo <72886034+AndradeJV@users.noreply.github.com>
2022-04-11 18:59:58 -0300

    Route Activities updated

---
+++ cypress/integration/GET/Activities.spec.js
@@ -0,0 +1,23 @@
+import Activities from "../../requests/Activities.request";
+
+const payloadAllActivities = require("../../fixtures/Activities/GET/listAllActivities.json");
+const payloadActivity = require("../../fixtures/Activities/GET/Activity.json");
+
+
+describe('GET - Endpoint Activities', () => {
+    it('Should to return success - All activities', () => {
+        Activities.getAllActivities().should(response => {
+            if(expect(response.status).to.equal(200)){
+                expect(response.body[0].id).to.equal(payloadAllActivities.success.responseBody.id);
+                expect(response.body[0].title).to.equal(payloadAllActivities.success.responseBody.title);
+            }
+        });
+    });
+
+    it('Should to return success - Activity', () => {
+        Activities.getActivity(payloadActivity.success.responseBody10.id).should(response => {
+            expect(response.status).to.not.equal(400);
+            expect(response.body.title).to.equal(payloadActivity.success.responseBody10.title);
+        });
+    });
+})
---
+++ cypress/integration/PUT/Activities.spec.js
@@ -0,0 +1,20 @@
+import Activities from "../../requests/Activities.request";
+const payload = require("../../fixtures/Activities/PUT/putBook.json");
+
+
+describe("PUT - Endpoint Activities", () => {
+    before(() => {
+        Activities.getActivity(payload.sendSuccess.id).should(response => {
+            expect(response.body.completed).to.equal(payload.sendSuccess.bodyNotPut.completed);
+            expect(response.body.title).to.equal(payload.sendSuccess.bodyNotPut.title);
+        });
+    });
+
+    it("Should to return success", () => {
+        Activities.putActivities(payload.sendSuccess.id, payload.sendSuccess.body).should(response => {
+            expect(response.status).to.equal(payload.sendSuccess.statusCode);
+            expect(response.body.completed).to.equal(payload.sendSuccess.body.completed);
+        });
+    });
+})
+
